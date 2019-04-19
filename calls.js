780ef904b4d26a5db0f3906da584bc4c28c25b18
tktommykline <tktommykline@gmail.com>
2019-04-18 22:15:01 -0400

    Initial commit

---
+++ calls.js
@@ -0,0 +1,9 @@
+//Bands in Town key = `app_id=codingbootcamp`OMDB key = `trilogy`
+//create folder
+//cd into it, then create files
+//npm init -y
+//you will need axios and spotify, but that is in the instructions
+//first, tackle the api calls that you have keys for
+//just hard code examples and run them'
+//then try to take user input and call them with that
+//then go on to spotify stuff
